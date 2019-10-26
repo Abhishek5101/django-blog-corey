7624c35960dbd9f027eb895048cc530ef486c077
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-10-26 12:45:39 -0700

    templates added for blog app views

--- django_corey_schafer/blog/views.py
+++ django_corey_schafer/blog/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-	return HttpResponse('Home')
+	return render(request, 'blog/home.html')
 
 
 def about(request):
-	return HttpResponse('ABout')
+	return render(request, 'blog/about.html')
